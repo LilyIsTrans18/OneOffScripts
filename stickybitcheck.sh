e80b0131b1b3e0c25fd3a4ab74bcf42b2e633759
LilyIsTrans <47757916+LilyIsTrans18@users.noreply.github.com>
2025-09-02 14:37:30 -0500

    Create stickybitcheck.sh

---
+++ stickybitcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ls -Rl / 2> /dev/null | grep rws | grep -vF \.rws > ~/tmpSB
+md5one="$(md5sum ~/stickybits | awk '{ print $1 }' )"
+md5two="$(md5sum ~/tmpSB | awk '{ print $1 }' )"
+
+if [ "$md5one" != "$md5two" ]; then
+touch ~/Desktop/stickybitAlert
+notify-send "stickybitschanged"
+mv ~/tmpSB ~/stickybitsCHANGED
+exit
+fi
+#if no sticky bits changed, remove the temporary file. otherwise keep it so user can look at it
+rm ~/tmpSB
